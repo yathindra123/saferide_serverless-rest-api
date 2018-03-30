d90c5f0420cd0a2efa6ac9da014180897c352248
PEROOT\URAWYYA <yathindra.rawya@pearson.com>
2018-03-30 22:58:43 +0530

    Initial commit

---
+++ api/create.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.create = (event, context, callback) => {
+
+    const timestamp = new Date().getTime();
+    const data = JSON.parse(event.body);
+
+    if (typeof data.name !== 'string') {
+        console.error('Validation Failed');
+        callback(null, {
+            statusCode: 400,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'Couldn\'t create the item.',
+        });
+        return;
+    }
+
+    const params = {
+        TableName: process.env.DYNAMODB_TABLE,
+        Item: {
+            id: uuid.v1(),
+            name: data.name,
+            email: data.email,
+            createdAt: timestamp,
+            updatedAt: timestamp,
+        },
+    };
+
+    // write to the database
+    dynamoDb.put(params, (error) => {
+        // handle potential errors
+        if (error) {
+            console.error(error);
+            callback(null, {
+                statusCode: error.statusCode || 501,
+                headers: { 'Content-Type': 'text/plain' },
+                body: 'Couldn\'t create the item.',
+            });
+            return;
+        }
+
+        // create a response
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(params.Item),
+        };
+        callback(null, response);
+    });
+};
